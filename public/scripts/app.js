a9487c62761204a3df75f22b6bfd80109dd7dce3
HarshaKy <harshaky6573@gmail.com>
2019-03-25 01:42:45 +0530

    rendering templates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,54 @@
 console.log('App.js is running');
 
 var template = React.createElement(
-  'h1',
-  { id: 'testID' },
-  'Decision App'
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		{ id: 'testID' },
+		'Decision App'
+	),
+	React.createElement(
+		'p',
+		null,
+		'This is some info'
+	),
+	React.createElement(
+		'ol',
+		null,
+		React.createElement(
+			'li',
+			null,
+			'One'
+		),
+		React.createElement(
+			'li',
+			null,
+			'Two'
+		)
+	)
 );
+
+var testTemplate = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		'Harsha Ky'
+	),
+	React.createElement(
+		'p',
+		null,
+		'Age: 21'
+	),
+	React.createElement(
+		'p',
+		null,
+		'Location: Bangalore, India'
+	)
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(testTemplate, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 console.log('App.js is running')
 
-var template = <h1 id="testID">Decision App</h1>
+var template = (
+<div>
+	<h1 id="testID">Decision App</h1>
+	<p>This is some info</p>
+	<ol>
+		<li>One</li>
+		<li>Two</li>
+	</ol>
+</div>
+)
+
+var testTemplate = (
+	<div>
+		<h1>Harsha Ky</h1>
+		<p>Age: 21</p>
+		<p>Location: Bangalore, India</p>
+	</div>
+)
+
 var appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(testTemplate, appRoot)
