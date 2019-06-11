7782fe3ad94fa142e48f81503dc3bab901fc2546
HarshaKy <harshaky6573@gmail.com>
2019-06-11 16:08:49 +0530

    pick what to do

--- src/app.js
+++ src/app.js
@@ -23,10 +23,14 @@ const onRemoveAll = () => {
 	render()
 }
 
+const onMakeDecision = () => {
+	const randomNum = Math.floor(Math.random() * app.options.length)
+	const option = app.options[randomNum]
+	alert(option)
+	// console.log(option)
+}
 
 const appRoot = document.getElementById('app')
-
-const numbers = [55, 101, 1000]
 
 const render = () => {
 	const template = (
@@ -34,15 +38,12 @@ const render = () => {
 			<h1>{app.title}</h1>
 			{app.subtitle && <p>{app.subtitle}</p>}
 			<p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
-			<p>{app.options.length}</p>
-
+			<button disabled={app.options.length === 0} onClick={onMakeDecision}>What Should I Do?</button>
 			<button onClick={onRemoveAll}>Remove All</button>
 
 			<ol>
 				{
-					app.options.map((option) => {
-						return <li key={option}>{option}</li>
-					})
+					app.options.map((option) => <li key={option}>{option}</li>)
 				}
 			</ol>
 
