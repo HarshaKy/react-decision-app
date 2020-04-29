6930298ed305199400591cfc9da06bfb6e4264f4
HarshaKy <harshaky6573@gmail.com>
2020-04-30 01:54:49 +0530

    nested components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+	render() {
+		return (
+			<div>
+				<Header />
+				<Action />
+				<Options />
+				<AddOption />
+			</div>
+		)
+	}
+}
+
 class Header extends React.Component {
 	render() {
 		return (
@@ -23,7 +36,18 @@ class Options extends React.Component {
 	render() {
 		return (
 			<div>
-				<p>Here are your options</p>
+				Here are your options
+				<Option />
+			</div>
+		)
+	}
+}
+
+class Option extends React.Component {
+	render() {
+		return (
+			<div>
+				Option component Here
 			</div>
 		)
 	}
@@ -39,13 +63,4 @@ class AddOption extends React.Component {
 	}
 }
 
-const jsx = (
-	<div>
-		<Header />
-		<Action />
-		<Options />
-		<AddOption />
-	</div>
-)
-
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
