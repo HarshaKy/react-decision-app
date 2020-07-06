b94ab028ed7472cd2baa590ede06587bdb3ffc1d
HarshaKy <harshaky6573@gmail.com>
2020-07-07 00:35:10 +0530

    split into different components

---
+++ src/components/Options.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Option from './Option'
+
+const Options = (props) => {
+	return (
+		<div>
+			{
+				props.options.map((option) => (
+					<Option 
+						key={option} 
+						optionText={option}
+						handleDeleteOption={props.handleDeleteOption}
+					/>
+				))
+			}
+
+			<button 
+				onClick={props.handleDeleteOptions}
+				disabled={!props.hasOptions}
+			>
+				Remove All
+			</button>
+
+			{!props.hasOptions && <p>Add an option to get started</p>}
+		</div>
+	)
+}
+
+export default Options
