78f85e638b39ff5026f97adbcd4d5bcfff8b4357
HarshaKy <harshaky6573@gmail.com>
2021-01-11 01:04:16 +0530

    refactoring stateless functional components

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,29 +1,27 @@
 import React from 'react'
 import Option from './Option'
 
-const Options = (props) => {
-	return (
-		<div>
-			{
-				props.options.map((option) => (
-					<Option 
-						key={option} 
-						optionText={option}
-						handleDeleteOption={props.handleDeleteOption}
-					/>
-				))
-			}
+const Options = (props) => (
+	<div>
+		{
+			props.options.map((option) => (
+				<Option
+					key={option}
+					optionText={option}
+					handleDeleteOption={props.handleDeleteOption}
+				/>
+			))
+		}
 
-			<button 
-				onClick={props.handleDeleteOptions}
-				disabled={!props.hasOptions}
-			>
-				Remove All
-			</button>
+		<button
+			onClick={props.handleDeleteOptions}
+			disabled={!props.hasOptions}
+		>
+			Remove All
+		</button>
 
-			{!props.hasOptions && <p>Add an option to get started</p>}
-		</div>
-	)
-}
+		{!props.hasOptions && <p>Add an option to get started</p>}
+	</div>
+)
 
 export default Options
